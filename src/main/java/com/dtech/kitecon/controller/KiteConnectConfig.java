f77e5267e7fc13907735b3b158224e3550a323e0
Anand Vaidya <anand1711@gmail.com>
2020-09-07 22:55:12 +0530

    externalize the porperties config.

--- src/main/java/com/dtech/kitecon/controller/KiteConnectConfig.java
+++ src/main/java/com/dtech/kitecon/controller/KiteConnectConfig.java
@@ -4,6 +4,7 @@ import com.zerodhatech.kiteconnect.KiteConnect;
 import com.zerodhatech.kiteconnect.kitehttp.exceptions.KiteException;
 import com.zerodhatech.models.User;
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -12,10 +13,19 @@ import java.io.IOException;
 @Getter
 public class KiteConnectConfig {
 
+    @Value("${kite.api.key}")
+    private String apiKey;
+
+    @Value("${kite.api.user}")
+    private String userId;
+
+    @Value("${kite.api.secret}")
+    private String secret;
+
     private KiteConnect kiteConnect;
 
     public final void initialize(String requestToken) throws KiteException, IOException {
-        this.kiteConnect = new KiteConnect("1g19o5ohebi8fj3l");
+        this.kiteConnect = new KiteConnect(apiKey);
 
         //If you wish to enable debug logs send true in the constructor, this will log request and response.
         //KiteConnect kiteConnect = new KiteConnect("xxxxyyyyzzzz", true);
@@ -24,12 +34,12 @@ public class KiteConnectConfig {
         //KiteConnect kiteConnect = new KiteConnect("xxxxyyyyzzzz", userProxy, false);
 
         // Set userId
-        kiteConnect.setUserId("ZQ5356");
+        kiteConnect.setUserId(userId);
 
         // Get login url
         String url = kiteConnect.getLoginURL();
 
-        User user = kiteConnect.generateSession(requestToken, "47hfn5uwrb138506whg0lk26w6pxiadi");
+        User user = kiteConnect.generateSession(requestToken, secret);
         kiteConnect.setAccessToken(user.accessToken);
         kiteConnect.setPublicToken(user.publicToken);
     }
