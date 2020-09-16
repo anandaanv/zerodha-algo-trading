8b2faa728fe3bd7386805581ecff0956a8573030
Anand Vaidya <anand1711@gmail.com>
2020-09-16 19:09:34 +0530

    Bunch of refactors to cleanup data download. Download by making multiple  requests with small timeframe, instead one large frame.. add 5 minutes candles download. refactor the logic to make it more cleaner.

---
+++ src/main/java/com/dtech/kitecon/data/FiveMinuteCandle.java
@@ -0,0 +1,18 @@
+package com.dtech.kitecon.data;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class FiveMinuteCandle extends BaseCandle {
+
+  @Builder
+  public FiveMinuteCandle(Double open, Double high, Double low, Double close, long volume, long oi, LocalDateTime timestamp, Instrument instrument) {
+    super(open, high, low, close, volume, oi, timestamp, instrument);
+  }
+}
---
+++ src/main/java/com/dtech/kitecon/service/DateRange.java
@@ -0,0 +1,33 @@
+package com.dtech.kitecon.service;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@AllArgsConstructor
+@Builder
+@Data
+public class DateRange {
+  private ZonedDateTime startDate;
+  private ZonedDateTime endDate;
+
+  public List<DateRange> split(int days) {
+    List<DateRange> result = new ArrayList<>();
+    ZonedDateTime startRef = startDate;
+    ZonedDateTime endRef = startDate.plusDays(days);
+    while (endRef.isBefore(endDate)) {
+      result.add(DateRange.builder()
+          .endDate(endRef)
+          .startDate(startRef).build());
+      startRef = endRef;
+      endRef = startRef.plusDays(days);
+    }
+    result.add(DateRange.builder()
+        .endDate(endDate)
+        .startDate(startRef).build());
+    return result;
+  }
+}
