061dfedf50f1eaf5fd462a1635725d6218a78f10
Anand Vaidya <anand1711@gmail.com>
2020-09-25 16:31:05 +0530

    A quick PnL calculator that will calculate extrapolated PnL based on zerodha margin and brokerage calculations

--- src/test/java/com/dtech/kitecon/service/PnLCalculator.java
+++ src/test/java/com/dtech/kitecon/service/PnLCalculator.java
@@ -6,33 +6,33 @@ public class PnLCalculator {
 
   @Test
   void calculatePnL() {
-    Double amount = 50000.0;
-    Double tentativePrice = 240.0;
-    Double marginPercentage = 21.0;
-    Double buyProfit = 250.0;
-    Double sellProfit = 118.0;
-    Double brokerage = 138.0;
+    double amount = 50000.0;
+    double tentativePrice = 240.0;
+    double marginPercentage = 21.0;
+    double buyProfit = 250.0;
+    double sellProfit = 118.0;
+    double brokerage = 138.0;
 
-    Integer numberTrades = 298 + 275;
+    int numberTrades = 298 + 275;
 
     System.out.println();
 
-    Double availableMargin = amount * 100 / marginPercentage;
+    double availableMargin = amount * 100 / marginPercentage;
     System.out.println("available margin     = " + Math.floor(availableMargin));
 
-    Double volume = availableMargin / tentativePrice;
+    double volume = availableMargin / tentativePrice;
     System.out.println("recommended volume   = " + Math.floor(volume));
 
-    Double totalBrokerage = numberTrades * brokerage;
+    double totalBrokerage = numberTrades * brokerage;
     System.out.println("total brokerage      = " + Math.floor(totalBrokerage));
 
-    Double ledgerProfit = ( buyProfit + sellProfit) * volume;
+    double ledgerProfit = ( buyProfit + sellProfit) * volume;
     System.out.println("ledger profit        = " + Math.floor(ledgerProfit));
 
-    Double actualProfit = ledgerProfit - totalBrokerage;
+    double actualProfit = ledgerProfit - totalBrokerage;
     System.out.println("actual profit        = " + Math.floor(actualProfit));
 
-    Double profitPercentage = actualProfit * 100 / amount;
+    double profitPercentage = actualProfit * 100 / amount;
     System.out.println("profit percentage    = " + Math.floor(profitPercentage));
 
     System.out.println();
