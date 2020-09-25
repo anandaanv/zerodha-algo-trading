353d36cd7a9da9ccb8c6f3aa5f258cdff3df3b60
Anand Vaidya <anand1711@gmail.com>
2020-09-25 16:32:12 +0530

    First level of refactor on strategy builder. Now it should be taken to the database.

--- src/main/java/com/dtech/kitecon/strategy/builder/OpeningRangeBreakoutStrategyBuider.java
+++ src/main/java/com/dtech/kitecon/strategy/builder/OpeningRangeBreakoutStrategyBuider.java
@@ -3,105 +3,83 @@ package com.dtech.kitecon.strategy.builder;
 import com.dtech.kitecon.data.Instrument;
 import com.dtech.kitecon.strategy.TradeDirection;
 import java.util.Map;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseStrategy;
-import org.ta4j.core.Rule;
 import org.ta4j.core.Strategy;
-import org.ta4j.core.indicators.EMAIndicator;
-import org.ta4j.core.indicators.SMAIndicator;
-import org.ta4j.core.indicators.candles.BearishEngulfingIndicator;
-import org.ta4j.core.indicators.candles.BearishHaramiIndicator;
-import org.ta4j.core.indicators.candles.BullishEngulfingIndicator;
-import org.ta4j.core.indicators.candles.BullishHaramiIndicator;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
-import org.ta4j.core.indicators.helpers.ConstantIndicator;
-import org.ta4j.core.indicators.helpers.DifferenceIndicator;
-import org.ta4j.core.indicators.helpers.DifferencePercentage;
 import org.ta4j.core.indicators.helpers.DifferenceRatioIndicator;
-import org.ta4j.core.indicators.helpers.HighPriceIndicator;
-import org.ta4j.core.indicators.helpers.LowPriceIndicator;
-import org.ta4j.core.indicators.helpers.OpenPriceIndicator;
 import org.ta4j.core.indicators.pivotpoints.TimeLevel;
 import org.ta4j.core.indicators.range.LastCandleOfPeriod;
 import org.ta4j.core.indicators.range.OpeningRangeHigh;
 import org.ta4j.core.indicators.range.OpeningRangeLow;
 import org.ta4j.core.indicators.range.PercentageIndicator;
-import org.ta4j.core.indicators.range.TimeOfCandle;
-import org.ta4j.core.num.DoubleNum;
-import org.ta4j.core.num.Num;
 import org.ta4j.core.num.PrecisionNum;
-import org.ta4j.core.trading.rules.BooleanIndicatorRule;
-import org.ta4j.core.trading.rules.BooleanRule;
 import org.ta4j.core.trading.rules.CrossedDownIndicatorRule;
 import org.ta4j.core.trading.rules.CrossedUpIndicatorRule;
 import org.ta4j.core.trading.rules.IsEqualRule;
-import org.ta4j.core.trading.rules.NotRule;
-import org.ta4j.core.trading.rules.OverIndicatorRule;
 import org.ta4j.core.trading.rules.StopGainRule;
 import org.ta4j.core.trading.rules.TrailingStopLossRule;
 import org.ta4j.core.trading.rules.UnderIndicatorRule;
-import org.ta4j.core.trading.rules.WithinPercentageRule;
-import org.ta4j.core.trading.rules.WithinRangeRule;
 
 @Component
 public class OpeningRangeBreakoutStrategyBuider extends BaseStrategyBuilder {
 
-  private static int orbLimit = 2;
-  private static double falseBreakoutOffset = 0.01;
+  public static final int TRAILING_STOPLOSS_PERCENTAGE = 1;
+  public static final double SIZE_OF_OPENING_CANDLE = 3;
+  private static int NUM_OPENING_CANDLES = 2;
+  private static double falseBreakoutOffset = 0.03;
+  private static int gainPercentage = 5;
 
   private static Strategy createOpeningPriceBreakoutBullish(BarSeries series) {
-    OpeningRangeHigh high = new OpeningRangeHigh(series, TimeLevel.DAY, orbLimit);
-    PercentageIndicator entry = new PercentageIndicator(high, PrecisionNum.valueOf(falseBreakoutOffset));
-    OpeningRangeLow low = new OpeningRangeLow(series, TimeLevel.DAY, orbLimit);
-    PercentageIndicator exit = new PercentageIndicator(low,PrecisionNum.valueOf(0 - falseBreakoutOffset));
+    PercentageIndicator openingRangeHigh = openingRangeHigh(series);
+    PercentageIndicator openingRangeLow = openingRangeLow(series);
     LastCandleOfPeriod last = new LastCandleOfPeriod(series, TimeLevel.DAY, 2);
     ClosePriceIndicator close = new ClosePriceIndicator(series);
-    HighPriceIndicator highPrice = new HighPriceIndicator(series);
-    EMAIndicator ema450 = new EMAIndicator(close, 450);
-    SMAIndicator sma50 = new SMAIndicator(close, 450);
-    OpenPriceIndicator open = new OpenPriceIndicator(series);
-
-//    Rule emaRule = new WithinPercentageRule(ema50, close, PrecisionNum.valueOf(1));
-//    Rule emaWithinHighLowRule = new WithinRangeRule(entry, exit,ema450);
-    Rule ema50WithinHighLowRule = new WithinRangeRule(entry, exit,ema450);
 
     return new BaseStrategy(
-        new CrossedUpIndicatorRule(close, entry)
-            .and(new UnderIndicatorRule(new DifferenceRatioIndicator(entry, exit), PrecisionNum.valueOf(2.5))),
-//            .and(new NotRule(emaWithinHighLowRule))
-//            .and(new NotRule(emaRule)),
-        new CrossedDownIndicatorRule(close, exit)
-            .or(new TrailingStopLossRule(close, PrecisionNum.valueOf(1)))
-            .or(new StopGainRule(close, 4))
+        new CrossedUpIndicatorRule(close, openingRangeHigh)
+            .and(verifySizeOfOpeningCandle(openingRangeHigh, openingRangeLow)),
+        new CrossedDownIndicatorRule(close, openingRangeLow)
+            .or(new TrailingStopLossRule(close, PrecisionNum.valueOf(TRAILING_STOPLOSS_PERCENTAGE)))
+            .or(new StopGainRule(close, gainPercentage))
             .or(new IsEqualRule(close, last))
     );
   }
 
+  @NotNull
+  private static UnderIndicatorRule verifySizeOfOpeningCandle(PercentageIndicator openingRangeHigh,
+      PercentageIndicator openingRangeLow) {
+    return new UnderIndicatorRule(new DifferenceRatioIndicator(openingRangeHigh, openingRangeLow),
+        PrecisionNum.valueOf(SIZE_OF_OPENING_CANDLE));
+  }
+
+  @NotNull
+  private static PercentageIndicator openingRangeLow(BarSeries series) {
+    OpeningRangeLow low = new OpeningRangeLow(series, TimeLevel.DAY, NUM_OPENING_CANDLES);
+    return new PercentageIndicator(low, PrecisionNum.valueOf(0 - falseBreakoutOffset));
+  }
+
   private static Strategy createOpeningPriceBreakoutBearish(BarSeries series) {
-    OpeningRangeHigh high = new OpeningRangeHigh(series, TimeLevel.DAY, orbLimit);
-    PercentageIndicator entry = new PercentageIndicator(high, PrecisionNum.valueOf(falseBreakoutOffset));
-    OpeningRangeLow low = new OpeningRangeLow(series, TimeLevel.DAY, orbLimit);
-    PercentageIndicator exit = new PercentageIndicator(low,PrecisionNum.valueOf(0 - falseBreakoutOffset));
+    PercentageIndicator openingRangeHigh = openingRangeHigh(series);
+    PercentageIndicator openingRangeLow = openingRangeLow(series);
     LastCandleOfPeriod last = new LastCandleOfPeriod(series, TimeLevel.DAY, 2);
     ClosePriceIndicator close = new ClosePriceIndicator(series);
-    LowPriceIndicator lowPriceIndicator = new LowPriceIndicator(series);
-    EMAIndicator ema450 = new EMAIndicator(close, 450);
-    EMAIndicator ema50 = new EMAIndicator(close, 50);
-    SMAIndicator sma50 = new SMAIndicator(close, 450);
-    Rule emaRule = new WithinPercentageRule(ema50, close, PrecisionNum.valueOf(1));
-    Rule emaWithinHighLowRule = new WithinRangeRule(entry, exit,ema450);
-
     return new BaseStrategy(
-        new CrossedDownIndicatorRule(close, exit)
-            .and(new UnderIndicatorRule(new DifferenceRatioIndicator(entry, exit), PrecisionNum.valueOf(2.5))),
-//            .and(new NotRule(emaWithinHighLowRule))
-//            .and(new NotRule(emaRule)),
-        new CrossedUpIndicatorRule(close, exit)
-            .or(new TrailingStopLossRule(close, PrecisionNum.valueOf(1)))
-            .or(new StopGainRule(close, 4))
+        new CrossedDownIndicatorRule(close, openingRangeLow)
+            .and(verifySizeOfOpeningCandle(openingRangeHigh, openingRangeLow)),
+        new CrossedUpIndicatorRule(close, openingRangeHigh)
+            .or(new TrailingStopLossRule(close, PrecisionNum.valueOf(TRAILING_STOPLOSS_PERCENTAGE)))
+            .or(new StopGainRule(close, gainPercentage))
             .or(new IsEqualRule(close, last))
     );
+  }
+
+  @NotNull
+  private static PercentageIndicator openingRangeHigh(BarSeries series) {
+    OpeningRangeHigh high = new OpeningRangeHigh(series, TimeLevel.DAY, NUM_OPENING_CANDLES);
+    return new PercentageIndicator(high, PrecisionNum.valueOf(falseBreakoutOffset));
   }
 
   @Override
