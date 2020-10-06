c223c16c38d020561944546c0bad4fd075ed3cc6
Anand Vaidya <anand1711@gmail.com>
2020-10-06 20:49:48 +0530

    Nested Rules with and/or/xor
    We are now ready to kickoff rule building and strategy building.

--- src/test/java/com/dtech/algo/strategy/units/CachedRuleBuilderTest.java
+++ src/test/java/com/dtech/algo/strategy/units/CachedRuleBuilderTest.java
@@ -7,6 +7,8 @@ import com.dtech.algo.rules.RuleRegistry;
 import com.dtech.algo.strategy.builder.cache.ConstantsCache;
 import com.dtech.algo.strategy.builder.cache.IndicatorCache;
 import com.dtech.algo.strategy.builder.cache.RuleCache;
+import com.dtech.algo.strategy.config.FollowUpRuleConfig;
+import com.dtech.algo.strategy.config.FollowUpRuleType;
 import com.dtech.algo.strategy.config.RuleConfig;
 import com.dtech.algo.strategy.config.RuleInput;
 import com.dtech.algo.strategy.config.RuleInputType;
@@ -14,6 +16,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import java.util.Arrays;
+import java.util.Collections;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -73,7 +76,7 @@ class CachedRuleBuilderTest {
   }
 
   @Test
-  void getCrossedUpRule() throws JsonProcessingException, StrategyException {
+  void underIndicatorRule() throws JsonProcessingException, StrategyException {
     String closePriceIndicatorName = "close1";
     String closeOffset = "close-offset";
     Mockito.doReturn(this.closePriceIndicator)
@@ -94,6 +97,60 @@ class CachedRuleBuilderTest {
     config.setInputs(Arrays.asList(input, num));
     System.out.println(objectMapper.writeValueAsString(config));
     Rule rule = ruleBuilder.getRule(config);
+    Boolean value = rule.isSatisfied(10);
+    assertTrue(value);
+  }
+
+  @Test
+  void followUpSingleRule() throws JsonProcessingException, StrategyException {
+    String closePriceIndicatorName = "close1";
+    String number12 = "number-12";
+    String number8 = "number-8";
+    Mockito.doReturn(this.closePriceIndicator)
+        .when(indicatorCache).get(closePriceIndicatorName);
+    Mockito.doReturn(PrecisionNum.valueOf(10.0))
+        .when(closePriceIndicator).getValue(10);
+    Mockito.doReturn("12")
+        .when(constantsCache).get(number12);
+    Mockito.doReturn("8")
+        .when(constantsCache).get(number8);
+
+    // close price indicator
+    RuleInput closePrice = new RuleInput();
+    closePrice.setName("close1");
+    closePrice.setType(RuleInputType.Indicator);
+
+    // under indicator rule
+    RuleConfig underIndicatorConfig = new RuleConfig();
+    underIndicatorConfig.setKey(closePriceIndicatorName);
+    underIndicatorConfig.setRuleName("under-indicator-rule");
+    // constant 12
+    RuleInput num12 = new RuleInput();
+    num12.setName(number12);
+    num12.setType(RuleInputType.Number);
+    // build rule
+    underIndicatorConfig.setInputs(Arrays.asList(closePrice, num12));
+
+    // Over indicator Config
+    RuleConfig overIndicatorConfig = new RuleConfig();
+    overIndicatorConfig.setKey(closePriceIndicatorName);
+    overIndicatorConfig.setRuleName("over-indicator-rule");
+    // constant 8
+    RuleInput num8 = new RuleInput();
+    num8.setName(number8);
+    num8.setType(RuleInputType.Number);
+    // build rule
+    overIndicatorConfig.setInputs(Arrays.asList(closePrice, num8));
+    FollowUpRuleConfig followUpRule = FollowUpRuleConfig.builder()
+        .followUpRule(overIndicatorConfig)
+        .followUpRuleType(FollowUpRuleType.And)
+        .build();
+    underIndicatorConfig.setFollowUpRules(Collections.singletonList(followUpRule));
+
+    //print rule
+    System.out.println(objectMapper.writeValueAsString(underIndicatorConfig));
+
+    Rule rule = ruleBuilder.getRule(underIndicatorConfig);
     Boolean value = rule.isSatisfied(10);
     assertTrue(value);
   }
