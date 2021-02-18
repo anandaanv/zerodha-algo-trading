a0dc04f2ce6d181514f7553de90cd920184bc34e
Anand Vaidya <anand1711@gmail.com>
2021-02-18 20:52:37 +0530

    Extended Bar Series with test, in order to handle DataTicks

--- src/main/java/com/dtech/algo/runner/candle/UpdatableBarSeriesLoader.java
+++ src/main/java/com/dtech/algo/runner/candle/UpdatableBarSeriesLoader.java
@@ -1,5 +1,7 @@
 package com.dtech.algo.runner.candle;
 
-public interface UpdatableBarSeriesLoader {
+import com.dtech.algo.strategy.builder.ifc.BarSeriesLoader;
 
+public interface UpdatableBarSeriesLoader extends BarSeriesLoader {
+    void updateBarSeries(DataTick tick);
 }
