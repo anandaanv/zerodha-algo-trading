2c32c68f22e23609d11016e76e7fcbfc00b5faea
Anand Vaidya <anand@dheemantech.com>
2025-09-07 01:11:47 +0530

    integrate klinecharts instead of Tradingview lightweight charts

--- ui/chart-draw-app/src/main.tsx
+++ ui/chart-draw-app/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import ProApp from "./pro/ProApp";
 import "./styles.css";
 
 const root = createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(<ProApp />);
