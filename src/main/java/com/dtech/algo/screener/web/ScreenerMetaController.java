d4bda12bef3c70ddc8b5fdac29b558cc7d5d4c80
Anand Vaidya <anand@dheemantech.com>
2025-11-13 13:14:38 +0530

    fixing token issues

--- src/main/java/com/dtech/algo/screener/web/ScreenerMetaController.java
+++ src/main/java/com/dtech/algo/screener/web/ScreenerMetaController.java
@@ -15,7 +15,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api/screener-meta")
-@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"})
+@CrossOrigin(origins = {"*"})
 public class ScreenerMetaController {
 
     /**
--- src/main/java/com/dtech/kitecon/controller/ConfigController.java
+++ src/main/java/com/dtech/kitecon/controller/ConfigController.java
@@ -9,13 +9,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
+@RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@CrossOrigin(origins = {"*"})
 public class ConfigController {
 
   private final KiteConnectConfig kiteConnectConfig;
